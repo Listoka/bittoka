bffb459420a2cc905eac8ffe4558be1e762aca88
kels0la <samelsola@gmail.com>
2018-09-21 15:08:14 -0500

    added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,20 @@
-import React, { Component } from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Nav from "./components/Nav";
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <div>
+      <Nav />
+      {/* <Switch>
+        <Route exact path="/" component={Books} />
+        <Route exact path="/books" component={Books} />
+        <Route exact path="/books/:id" component={Detail} />
+        <Route component={NoMatch} />
+      </Switch> */}
+    </div>
+  </Router>
+);
 
 export default App;
---
+++ client/src/components/Nav/Nav.js
@@ -0,0 +1,31 @@
+import React from "react";
+import './Nav.css';
+
+const Nav = () => (
+  
+  <div className="container-fluid descriptionBox">
+    <nav className="navbar navbar-expand-lg navbar-light">
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+    </button>
+    {<a href="/"><span id="logoImage">Bittoka</span></a>/* <span className="typewriter"></span> */}
+    <div className="navbar-collapse collapse justify-content-end" id="navbarTogglerDemo03">
+    <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link hvr-glow" href="/jist">Jist <span className="sr-only">Jist</span></a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link hvr-glow" href="/join">Join <span className="sr-only">Jist</span></a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link hvr-glow" href="/sign-up">Login <span className="sr-only">Login</span></a>
+        </li>
+    </ul>
+    </div>
+    </nav>
+  </div>
+  
+  
+);
+
+export default Nav;
