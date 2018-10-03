3fe6f2a2fc1e672c2628e9bf1af221e5271cf5c3
kels0la <samelsola@gmail.com>
2018-10-03 15:54:44 -0500

    seeds link nav

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,45 +1,48 @@
-import React from "react";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import NavLoggedIn from "../NavLoggedIn";
+import NavNotLoggedIn from "../NavNotLoggedIn";
 import './Nav.css';
 import SignOut from "../SignOut";
-import AuthUserContext from '../AuthUserSession/AuthUserContext'
-import { auth } from '../../firebase'
+import AuthUserContext from '../AuthUserSession/AuthUserContext';
+import { auth } from '../../firebase';
 
-const logOutHandler = (event) => {
-  event.preventDefault()
-  auth.doSignOut()
-}
 
-const Nav = () => {
-  return (
-    <div className="container-fluid descriptionBox">
+//Will need to update a logged in / logged out state
+class Nav extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  logOutHandler = (event) => {
+    event.preventDefault()
+    auth.doSignOut()
+  }
+
+  render() {
+    return (
+      <div className="container-fluid descriptionBox">
       <nav className="navbar navbar-expand-lg navbar-light">
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <span id="logoImage"><a href="/">Bittoka</a></span>
+        <span id="logoImage"><Link to='/'>Listoka</Link></span>
         <div className="navbar-collapse collapse justify-content-end" id="navbarTogglerDemo03">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link hvr-glow" href="/gist">Gist <span className="sr-only">Jist</span></a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link hvr-glow" href="/join">Join <span className="sr-only">Jist</span></a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link hvr-glow" href="/login">Login <span className="sr-only">Login</span></a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link hvr-glow" href="/account">Account <span className="sr-only">Account</span></a>
-            </li>
-            <li className='nav-item'>
-              <a onClick={logOutHandler} className="nav-link hvr-glow" href="/SignOut">Sign Out <span className="sr-only">Sign Out</span></a>
-            </li>
-          </ul>
+            {/* This is the logic that determines if the user is logged in. Needs to be adjusted. */}
+            <AuthUserContext.Consumer>
+            {authUser => authUser ? (
+              <NavLoggedIn 
+                logOutHandler={this.logOutHandler}
+              />
+            ) : (
+              <NavNotLoggedIn />
+            )}
+            </AuthUserContext.Consumer>
         </div>
       </nav>
     </div>
-  )
-
-};
+    )
+  }
+}
 
 export default Nav;
