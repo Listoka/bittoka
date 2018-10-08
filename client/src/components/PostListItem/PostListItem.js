6230d845dec8b46929063ba56cdd31d61194d969
Andrew Vala <38055092+awvala@users.noreply.github.com>
2018-10-07 23:27:01 -0500

    basic animations

--- client/src/components/PostListItem/PostListItem.js
+++ client/src/components/PostListItem/PostListItem.js
@@ -5,10 +5,12 @@ import './postListItem.css';
 const PostListItem = props => {
     return (
         <div className='card listItem' id={props._id}>
-            <div className='card-title'><Link to={'/api/posts/' + props._id}><h4>{props.title}</h4></Link></div>
-            <h6 className='card-subtitle'>Posted by: {props.authorName}</h6>
-            <p className='card-text'>{props.body}</p>
+            <div className="card-body">
+                <h4 className='card-title'><Link to={'/api/posts/' + props._id}>{props.title}</Link></h4>
+                <h6 className='card-subtitle'>Posted by: {props.authorName}</h6>
+                <p className='card-text'>{props.body}</p>
             </div>
+        </div>
     );
 };
 
