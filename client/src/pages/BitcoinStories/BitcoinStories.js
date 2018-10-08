4efd3551120657b836e67a414bc31d4ce4edbd05
Andrew Vala <38055092+awvala@users.noreply.github.com>
2018-10-07 22:36:41 -0500

    Updated BD page

--- client/src/pages/BitcoinStories/BitcoinStories.js
+++ client/src/pages/BitcoinStories/BitcoinStories.js
@@ -6,6 +6,8 @@ import CategoryDescription from '../../components/CategoryDescription';
 import PostList from '../../components/PostList';
 import PostListItem from '../../components/PostListItem';
 import CreatePostButton from '../../components/CreatePostButton';
+import TagWrapper from '../../components/TagWrapper';
+import Tags from '../../components/Tags';
 
 class BitcoinStories extends Component {
   constructor(props) {
@@ -22,19 +24,19 @@ class BitcoinStories extends Component {
   componentDidMount() {
     this.getPosts(this.state.categoryName)
   }
-  
+
   getPosts = (categoryName) => {
     console.log(categoryName)
     API.getPostings(categoryName).then(results => {
       console.log(results.data);
-      this.setState({bitcoinStories: results.data});
+      this.setState({ bitcoinStories: results.data });
       this.getCategory(this.state.categoryName);
     });
   };
 
   getCategory = (categoryName) => {
     API.getCategoryInfo(categoryName).then(results => {
-      this.setState({ 
+      this.setState({
         displayName: results.data.displayName,
         description: results.data.description,
         tags: results.data.tags
@@ -45,21 +47,34 @@ class BitcoinStories extends Component {
 
   render() {
     return (
-      <div>
-        <div className="row">
-          <div className="col-xl-2">
-            {/* Tags/Subcategories would go here */}
+      <div className='pagebody'>
+        <div className='row'>
+          <div className='col-xl-2'></div>
+          <div className='col-xl-8'>
+            <CreatePostButton
+              categoryName={this.state.categoryName}
+            />
+          </div>
+          <div className='col-xl-2'></div>
+        </div>
+
+        <div className='row'>
+          <div className='col-xl-2'>
+            <TagWrapper>
+              {this.state.tags.map(tags => (
+                <Tags
+                  tag={tags}
+                />
+              ))}
+            </TagWrapper>
           </div>
 
-          <div className="col-xl-8">
-          <CreatePostButton 
-            categoryName={this.state.categoryName}
-          />
+          <div className='col-xl-8'>
             <CategoryDetail>
-            <CategoryDescription 
-              displayName={this.state.displayName}
-              description={this.state.description}
-            />
+              <CategoryDescription
+                displayName={this.state.displayName}
+                description={this.state.description}
+              />
               <PostList>
                 {this.state.bitcoinStories.map(bitcoinStory => (
                   <PostListItem
@@ -74,7 +89,7 @@ class BitcoinStories extends Component {
                     title={bitcoinStory.title}
                     _id={bitcoinStory._id}
                     author={bitcoinStory.author}//This is the numbers one. May not need
-                    // 
+                  // 
                   />
                 ))}
               </PostList>
