2cbec4812fce52fbe8fb64b4804fbed53201eef1
Timothy Smith <timsmith78@yahoo.com>
2018-10-11 00:58:24 -0500

    Added custom tip amount.  Submit valid tip amount, and then money button will appear.

--- client/src/components/PostComponents/PostDetail/PostDetail.js
+++ client/src/components/PostComponents/PostDetail/PostDetail.js
@@ -1,8 +1,17 @@
 import React, { Component } from "react";
 import CommentBox from '../../CommentBox';
 import TipButton from '../../../components/TipButton'
+import  { Input, FormBtn } from '../PostForm'
+
+
+const minTipAmt = .05
 
 export class PostDetail extends Component {
+    state = {
+        tipAmt: 0,
+        tipState: 0
+    }
+
     constructor(props) {
         super(props)
 
@@ -12,22 +21,46 @@ export class PostDetail extends Component {
         alert("Payment Successful!")
     }
 
+    handleTipChange = (evt) => {
+        this.setState( { tipState: evt.target.value })
+    }
+
+    handleTipSubmit = (evt) => {
+        evt.preventDefault()
+        this.setState( { tipAmt: this.state.tipState })
+    }
+
     render() {
         return (
             <div>
-            <br />
-            <p>{this.props.title}</p>
-            <p>By: {this.props.authorName}</p>
-            <p>{this.props.body}</p>
-            <TipButton
-                payeeId={this.props.author}
-                paymentAmt='.03'
-                label='Tip to Upvote'
-                paymentSuccessCbk={this.afterPayment}
-            />
-            <hr />
-            <CommentBox />
-        </div>            
+                <br />
+                <p>{this.props.title}</p>
+                <p>By: {this.props.authorName}</p>
+                <p>{this.props.body}</p>
+                {(this.state.tipAmt >= minTipAmt ?
+                    <TipButton
+                        payeeId={this.props.author}
+                        paymentAmt={this.state.tipAmt}
+                        label='Tip to Upvote'
+                        paymentSuccessCbk={this.afterPayment}
+                    />
+                    :
+                    <div>
+                    <p>To upvote, enter tip amount (minimum {minTipAmt}): </p>
+                    <Input
+                        onChange={this.handleTipChange} 
+                        className='form-control'
+                        />
+                    <FormBtn
+                        onClick={this.handleTipSubmit}
+                    >
+                    Submit Tip Amount
+                    </FormBtn>
+                    </div>
+                )}
+                <hr />
+                <CommentBox />
+            </div>
         )
     }
 }
