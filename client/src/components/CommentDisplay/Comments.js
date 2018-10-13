53f938ad6f60a40b36a6fa64ccd6536fc816c104
kels0la <samelsola@gmail.com>
2018-10-13 10:50:21 -0500

    login coming back

--- client/src/components/CommentDisplay/Comments.js
+++ client/src/components/CommentDisplay/Comments.js
@@ -5,37 +5,62 @@ export class Comments extends Component  {
     constructor (props) {
         super(props)
         this.state = {
-          isHidden: true
+          isHidden: true,
+          body: ""
         }
-      }
+      };
+
       toggleHidden () {
         this.setState({
           isHidden: !this.state.isHidden
         })
-      }
+      };
+
+      handleInputChange = event => {
+          console.log(this.state)
+        const { name, value } = event.target;
+        this.setState({
+          [name]: value
+        });
+      };
+
+    //   handleFormSubmit = (event) => {
+    //     console.log(this.state);
+    //     event.preventDefault();
+    //     const data = {
+    //         body: this.state.body,
+    //         commentPath: "commentPath"
+    //     }
+    //     if (this.state.body.length > 4) {
+    //         API.createComment(this.state.postID, data)
+    //         // .then( res => this.setState({ redirectToNewPage: true }))
+    //         .then (res => this.getPostWithComments())
+    //         .catch(err => console.log(err))
+    //     }
+    //   }
 
     render(){
-        console.log(props.createdAt)
-        const createdDate = props.createdAt && props.createdAt.slice(0, 10)
+        console.log(this.props.createdAt)
+        const createdDate = this.props.createdAt && this.props.createdAt.slice(0, 10)
         return (
             <React.Fragment>
-            <hr/>
-            <p><b>Author:</b> {this.props.authorName} [earned $x.xx]</p> 
-            <p><b>Comment:</b> {this.props.body}</p>
-            <p>[#Upvotes][Upvote MoneyButton Component]</p>
-            <p><button onClick={this.toggleHidden.bind(this)}>[Reply]</button></p>
-            <p>[Created on: {createdDate}]</p>
-            {!this.state.isHidden && <Child />}
+                <hr/>
+                <p><b>Author:</b> {this.props.authorName} [earned $x.xx]</p> 
+                <p><b>Comment:</b> {this.props.body}</p>
+                <p>[#Upvotes][Upvote MoneyButton Component]</p>
+                <p><button onClick={this.toggleHidden.bind(this)}>[Reply]</button></p>
+                <p>[Created on: {createdDate}]</p>
+                {!this.state.isHidden && <Child />}
             </React.Fragment>
         );
     }   
 };
 
-const Child = () => (
+const Child = (props) => (
     <React.Fragment>
         <TextArea 
         value=""
-        // onChange={this.handleInputChange}
+        onChange={this.handleInputChange}
         name="body"
         placeholder="Share your comment here"
         />
