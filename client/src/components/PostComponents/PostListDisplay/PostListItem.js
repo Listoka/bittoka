91e4b27c724628277ed9681dc8488c2331d0ce93
Rob Phillips <robbyphillips@gmail.com>
2018-10-14 16:30:44 -0500

    remove create post from home page, remove delete from post list item

--- client/src/components/PostComponents/PostListDisplay/PostListItem.js
+++ client/src/components/PostComponents/PostListDisplay/PostListItem.js
@@ -39,12 +39,7 @@ export const PostListItem = props => {
                                 authUser => {
                                     if (!!authUser && authUser.dbUser._id === props.author) {
                                         return (
-                                            <span className="delete-btn">
-                                                <span
-                                                    onClick={(event) => props.handleDeleteButton(event, props._id)}>
-                                                    <i className="far fa-trash-alt"></i>
-                                                </span>
-                                                &nbsp;&nbsp; | &nbsp;&nbsp;
+                                            <span className="edit-btn-float-right">
                                                 <span>
                                                     <Link to={{ pathname: `/posts/${props._id}/edit` }}>
                                                         <i className="far fa-edit"> Edit Post</i>
--- client/src/components/Stickybar/Stickybar.js
+++ client/src/components/Stickybar/Stickybar.js
@@ -8,29 +8,36 @@ import ReactTooltip from 'react-tooltip';
 const Stickybar = props => (
 
   <div className="icon-bar">
-  <Link className='facebook' to={{pathname:`/categories/${props.categoryName}/posts/new`}} data-tip data-for="createPost" data-offset="{'left': 15}"><i className="fas fa-pen-square"></i></Link>
-  <ReactTooltip id='createPost' place="left" type="dark" effect="solid" >
-   Create a post
+    {
+      props.categoryName
+        ? (
+          <React.Fragment>
+            <Link className='facebook' to={{ pathname: `/categories/${props.categoryName}/posts/new` }} data-tip data-for="createPost" data-offset="{'left': 15}"><i className="fas fa-pen-square"></i></Link>
+            <ReactTooltip id='createPost' place="left" type="dark" effect="solid" > Create a post </ReactTooltip>
+          </React.Fragment>
+        )
+        : null
+
+    }
+    {/* TODO: placeholders, not sure what we want to link to from here, but the page needed some color*/}
+    <a href="https://twitter.com/popmotionjs?lang=en" data-tip data-for='twitterToolTip' data-offset="{'left': 15}" className="twitter"><i className="fab fa-twitter"></i></a>
+    <ReactTooltip id='twitterToolTip' place="left" type="dark" effect="solid">
+      Follow us on Twitter
   </ReactTooltip>
-  {/* TODO: placeholders, not sure what we want to link to from here, but the page needed some color*/}
-  <a href="https://twitter.com/popmotionjs?lang=en" data-tip data-for='twitterToolTip'  data-offset="{'left': 15}" className="twitter"><i className="fab fa-twitter"></i></a> 
-  <ReactTooltip id='twitterToolTip' place="left" type="dark" effect="solid">
-    Follow us on Twitter
+    <a href="https://www.google.com/search?q=bitcoin&oq=bitcoin&aqs=chrome..69i57j0j69i60l2j0l2.5688j0j4&sourceid=chrome&ie=UTF-8" data-tip data-for='googleToolTip' data-offset="{'left': 15}" className="google"><i className="fab fa-google"></i></a>
+    <ReactTooltip id='googleToolTip' place="left" type="dark" effect="solid">
+      Google, if you must
   </ReactTooltip>
-  <a href="https://www.google.com/search?q=bitcoin&oq=bitcoin&aqs=chrome..69i57j0j69i60l2j0l2.5688j0j4&sourceid=chrome&ie=UTF-8" data-tip data-for='googleToolTip' data-offset="{'left': 15}" className="google"><i className="fab fa-google"></i></a> 
-  <ReactTooltip id='googleToolTip' place="left" type="dark" effect="solid">
-    Google, if you must
+    <a href="https://www.linkedin.com/feed/" data-tip data-for='linkedInToolTip' data-offset="{'left': 15}" className="linkedin"><i className="fab fa-linkedin"></i></a>
+    <ReactTooltip id='linkedInToolTip' place="left" type="dark" effect="solid">
+      Get a job ya hippie!
   </ReactTooltip>
-  <a href="https://www.linkedin.com/feed/" data-tip data-for='linkedInToolTip' data-offset="{'left': 15}" className="linkedin"><i className="fab fa-linkedin"></i></a>
-  <ReactTooltip id='linkedInToolTip' place="left" type="dark" effect="solid">
-    Get a job ya hippie!
-  </ReactTooltip>
-  <a href="https://youtu.be/o0u4M6vppCI" data-tip data-for='youtubeTooltip' data-offset="{'left': 15}" className="youtube"><i className="fab fa-youtube"></i></a> 
-  <ReactTooltip className="tooltip" id='youtubeTooltip' place="left" type="dark" effect="solid">
-    Cheap Netflix
+    <a href="https://youtu.be/o0u4M6vppCI" data-tip data-for='youtubeTooltip' data-offset="{'left': 15}" className="youtube"><i className="fab fa-youtube"></i></a>
+    <ReactTooltip className="tooltip" id='youtubeTooltip' place="left" type="dark" effect="solid">
+      Cheap Netflix
   </ReactTooltip>
 
-</div>
+  </div>
 );
 
 export default Stickybar;
