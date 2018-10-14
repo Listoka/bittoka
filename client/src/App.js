2a734302babe5f826ba366b601e25a3e7c9ce470
Rob Phillips <robbyphillips@gmail.com>
2018-10-14 11:56:40 -0500

    little fixes

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,11 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 //JSON file and navigation
-import {Nav} from './components/Nav';
+import { Nav } from './components/Nav';
 import categories from './categories.json';
 import SubNav from './components/subNav';
 import FlexContainer from './components/flexContainer';
-import API from './utils/API';
+// import API from './utils/API';
 //Routes
 import Home from './pages/Home';
 import CreatePost from './pages/CreatePost';
@@ -29,28 +29,28 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isOpen: false,
+      // isOpen: false,
       categories: categories,
     };
   };
 
-  toggle = () => this.setState({ isOpen: !this.state.isOpen });
-  
-  render(){
+  // toggle = () => this.setState({ isOpen: !this.state.isOpen });
+
+  render() {
     //console.log('state: ', this.state)
-    return(
+    return (
       <Router>
         <div>
           <Nav />
           <FlexContainer>
-          {this.state.categories.map(category => (
-            <SubNav 
-            id={category.id}
-            key={category.id}
-            href={category.href}
-            name={category.name}
-            />
-          ))}
+            {this.state.categories.map(category => (
+              <SubNav
+                id={category.id}
+                key={category.id}
+                href={category.href}
+                name={category.name}
+              />
+            ))}
           </FlexContainer>
           <Switch>
             <Route exact path='/' component={Home} />
@@ -63,8 +63,7 @@ class App extends Component {
             <Route exact path='/posts/:id' component={Content} />
             <Route exact path='/posts/:id/edit' component={EditPage} />
             <Route exact path='/account' component={AccountPage} />
-            <Route exact path='/authtest' component={authTest} />
-            <Route exact path='/postman' component={authTest} />
+            <Route exact path='/(authtest|postman)' component={authTest} />
             <Route component={NoMatch} />
           </Switch>
         </div>
