e601c0f6577c1ba061e7c9d9d502b2c1d1b1d1e7
Andrew Vala <38055092+awvala@users.noreply.github.com>
2018-10-14 15:23:15 -0500

    category flair and comments links

--- client/src/pages/NoMatch/NoMatch.js
+++ client/src/pages/NoMatch/NoMatch.js
@@ -2,14 +2,14 @@ import React from "react";
 
 const NoMatch = () => (
   <div className="pagebody">
-  <div>
-    <h1>404 Page Not Found</h1>
-    <h1>
-      <span role="img" aria-label="Face With Rolling Eyes Emoji">
-        🙄
+    <div>
+      <h1>404 Page Not Found</h1>
+      <h1>
+        <span role="img" aria-label="Face With Rolling Eyes Emoji">
+          🙄
       </span>
-    </h1>
-  </div>
+      </h1>
+    </div>
   </div>
 );
 
