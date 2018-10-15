e6e616c94adf246f41c7c122df1b8697caf9c108
Timothy Smith <timsmith78@yahoo.com>
2018-10-15 18:58:50 -0500

    Initial payment logging framework

--- client/src/components/PostComponents/PostDetail/PostDetail.js
+++ client/src/components/PostComponents/PostDetail/PostDetail.js
@@ -3,26 +3,38 @@ import TipButton from '../../../components/TipButton';
 import renderHTML from 'react-render-html'
 import { Link } from 'react-router-dom';
 import AuthUserContext from "../../AuthUserSession/AuthUserContext";
-import './postDetail.css';
+import API from '../../../utils/API'
+import './postDetail.css'
 
 export class PostDetail extends Component {
 
     constructor(props) {
         super(props)
         this.state = {
-            tipAmt: 0,
-            tipState: 0
-        }
+            body: props.body,
+            _id: props._id,
+            title: props.title,
+            teaser: props.teaser,
+            authorName: props.authorName,
+            categoryName: props.categoryName,
+            author: props.author,
+            upvotes: 0,
+            upvoteList: []
+        };
     }
 
-    afterPayment = () => {
-        alert("Payment Successful!")
+    afterPayment = (trans) => {
+        console.log(trans)
+        API.upvotePost(this.state._id).then(result => {
+
+        })
     };
 
     render() {
         return (
             <React.Fragment>
                 <br />
+
                 {/* TODO: refactor edit button into its own component using withAuthorization */}
                 <AuthUserContext.Consumer>
                     {
@@ -45,13 +57,21 @@ export class PostDetail extends Component {
                         {this.props.body ? renderHTML(this.props.body) : null}
                     </React.Fragment>
                 </div>
-                <TipButton
-                    minTipAmt='.03'
-                    tipMessage='UPVOTE'
-                    paymentSuccessCbk={this.afterPayment}
-                    label='Upvote'
-                    payeeId={this.props.author}
-                />
+                <div className='container'>
+                    <div className='row'>
+                        <TipButton
+                            minTipAmt='.03'
+                            tipMessage='UPVOTE'
+                            paymentSuccessCbk={this.afterPayment}
+                            label='Upvote'
+                            payeeId={this.props.author}
+                        />
+                        <div className='col-lg-3 col-md-4 col-sm-6 col-xs-6' >
+                            <h5>Upvotes: {this.state.upvotes}</h5>
+                            {this.state.upvoteList ? this.state.upvoteList.map(user => <p>{user.username}</p>) : null}
+                        </div>
+                    </div>
+                </div>
             </React.Fragment>
         );
     };
