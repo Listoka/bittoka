666099e6fbda0ceac3a743939b0888e040b317a3
kels0la <samelsola@gmail.com>
2018-10-17 20:37:38 -0500

    merging master

--- client/src/components/Modals/GistModal.js
+++ client/src/components/Modals/GistModal.js
@@ -23,13 +23,14 @@ const GistModal = props => {
             {/* <div className="gist-body">*/}
             <div className='row'>
                 <div className="col-lg-7">
-                    <p className='gist'>Listoka is a writing and entertainment based platform where users will also find and contribute
-                      to information that benefits people's lives. It will be similar to Reddit in the sense that users
-                      will be able to create posts, leave comments, and follow others. The difference is the topics are
-                      carefully constructed and chosen with purpose, and Bitcoin tips will be utilized as the main
-                      incentivization tool. That is, people will make money for their contributions, and the higher the
-                      quality of their contribution, the more they're likely to earn. Listoka aims to be a useful, fun,
-                                          money-making platform that becomes the gateway to Bitcoin for the average, everyday person.</p>
+                    <p className='gist'>Listoka is a writing and entertainment based platform where 
+                    users will also be able to find and contribute to information that benefits people's lives. 
+                    It will be similar to Reddit in the sense that users can create their own content, 
+                    leave feedback, and follow their favorite content creators. What sets Listoka apart is carefully 
+                    cultivated content and Bitcoin tip integration. That is, contributors will make money for their 
+                    contributions and the higher the quality of their content, the more they're likely to earn. Listoka 
+                    aims to be a useful, fun, money-making platform that becomes the gateway to Bitcoin for the average, 
+                    everyday person.</p>
                 </div>
                 <div className="col-lg-5">
                     <img className="gistImg img-fluid" src="./images/gistImg.jpg" alt="Young people working"></img>
