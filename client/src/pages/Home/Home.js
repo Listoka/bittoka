9eb53c322cc145fee306a756b7976016bc1b84f6
Andrew Vala <38055092+awvala@users.noreply.github.com>
2018-10-19 14:47:37 -0500

    added plus and minus before filtered tags

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -46,14 +46,14 @@ class Home extends Component {
         <div className='row'>
           <div className='col-sm-2'>
             <div className='homeTagWrapper rounded'>
-              <div className='homeSidebarWrapper'>
+              <div className='homeSidebarWrapper rounded'>
                 <img className="img-fluid" src="./images/newSidebar.jpg" alt="Welcome to Listoka"></img>
-                <div className="homeSideBar">
+                <div className="homeSideBar rounded">
                   <h6>Welcome to Listoka</h6>
-                  <p className='homeSidebarContent'>
-                    Interested in Bitcoin but you're not sure where to start?  We're here to help!
-                    All we ask is that you make the world a better place by sharing a story, your thoughts, or a little friendly advise.
-                  </p>
+                  <p className='homeSidebarContent'><strong>Write something, earn Bitcoin.  It's that simple.</strong></p>
+                    <p className='para'>Share a story, create a DIY project, or post your travelog.  Get paid to make Listoka a better place.  
+                        Upvote content you like and give the poster or commenter a $0.02 tip.  If you're really impressed, visit the author's profile page and leave a custom tip.
+                        Good content deserves support.  So get to to it!  </p>
                 </div>
               </div>
             </div>
