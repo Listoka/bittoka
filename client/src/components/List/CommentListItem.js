6caae92f4dc04f9d01564eb0238492eaa732ae23
Andrew Vala <38055092+awvala@users.noreply.github.com>
2018-11-28 22:37:51 -0600

    styled current comment view elements

--- client/src/components/List/CommentListItem.js
+++ client/src/components/List/CommentListItem.js
@@ -6,17 +6,16 @@ import { BitcoinIcon } from '../Widgets';
 export const CommentListItem = props => {
   return (
     <React.Fragment>
-      <div className='text-base m-10px px-10px py-5px border-l-2 border-solid rounded'>
-        <p className='text-sm mb-2'>
+      <div className='mb-20px p-40px rounded-8px bg-darkest-gray'>
+        <h4 className='mb-2'>
           <BitcoinIcon />
           <span>[$x.xx]</span>
-          <Link className='mr-1' to={{ pathname: `/users/${props.author}` }}> {props.authorName}</Link>
+          <Link className='mr-1' to={{ pathname: `/users/${props.author}` }} style={{color: '#FFF'}}> {props.authorName}</Link>
           <span> commented on TITLE {}</span>
           <Moment className='mr-2' fromNow>{props.createdAt}</Moment>
-        </p>
-        <p className='text-sm'> {props.body}</p>
+        </h4>
+        <p>{props.body}</p>
       </div>
-      <hr />
     </React.Fragment>
   );
 };
