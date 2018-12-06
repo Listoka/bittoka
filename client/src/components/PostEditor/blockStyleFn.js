beed8c5ef37fe48c3967ae7ab8ccd014fa30eee1
rob <robbyphillips@gmail.com>
2018-12-06 15:32:37 -0600

    small editor style tweak for unstyled blocks

--- client/src/components/PostEditor/blockStyleFn.js
+++ client/src/components/PostEditor/blockStyleFn.js
@@ -6,6 +6,7 @@ export default function blockStyleFn(block) {
     case 'blockquote': return 'italic mx-10 border-l-4 border-grey-darker p-2';
     case 'header-two': return 'text-black'
     case 'code-block': return 'mx-10 px-2 bg-grey-lighter'
+    case 'unstyled': return 'my-2'
     default: return null;
   }
 }
