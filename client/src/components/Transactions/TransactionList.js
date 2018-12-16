c19fa005099f8b7fd490e88dc3bdeb5506eeb97c
kels0la <samelsola@gmail.com>
2018-12-16 11:37:36 -0600

    stashing changes

--- client/src/components/Transactions/TransactionList.js
+++ client/src/components/Transactions/TransactionList.js
@@ -7,7 +7,7 @@ const TransactionList = props => {
       {console.log(props)}
       <div className='w-full bg-darkest-gray p-4 rounded-lg'>
         <table className='w-full text-left'>
-          <thead className=''>
+          <thead className='border-medium-gray border-b-2 hrModals mb-1'>
             <tr className='flex w-full'>
               <th className='p-1 w-1/4 font-normal font-base'>Date</th>
               <th className='p-1 w-1/4 font-normal font-base'>Type</th>
@@ -15,7 +15,6 @@ const TransactionList = props => {
               <th className='p-1 w-1/4 font-normal font-base'>To/From</th>
             </tr>
           </thead>
-          <hr className="border-medium-gray border-2 hrModals my-1"></hr>
         {/* <hr className="border-medium-gray border-2 hrModals mb-3"></hr> */}
           <tbody className='flex flex-col items-center justify-between w-full'>
             {props.transactions.map(transaction =>{
