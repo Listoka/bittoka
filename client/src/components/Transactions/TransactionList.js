1fff52f2c342d5d26b13c1c78dd506493dbab211
kels0la <samelsola@gmail.com>
2018-12-16 23:40:24 -0600

    Links added to transactions. Needs work

--- client/src/components/Transactions/TransactionList.js
+++ client/src/components/Transactions/TransactionList.js
@@ -8,16 +8,16 @@ const TransactionList = props => {
       {console.log(props)}
       <div className='w-full bg-darkest-gray p-4 rounded-lg'>
         <table className='w-full text-left'>
-          <thead className='border-medium-gray border-b-2 hrModals mb-3'>
+          <thead className='border-medium-gray border-b-2 hrModals'>
             <tr className='flex w-full'>
               <th className='p-1 w-1/4 font-normal font-base'>Date</th>
-              <th className='p-1 w-1/4 font-normal font-base'>Type</th>
               <th className='p-1 w-1/4 font-normal font-base'>Amount</th>
+              <th className='p-1 w-1/4 font-normal font-base'>Type</th>
               <th className='p-1 w-1/4 font-normal font-base'>To/From</th>
             </tr>
           </thead>
         {/* <hr className="border-medium-gray border-2 hrModals mb-3"></hr> */}
-          <tbody className='flex flex-col items-center justify-between w-full'>
+          <tbody className='flex flex-col items-center justify-between w-full mt-2'>
             {props.transactions.map(transaction =>{
               return (
               <TxListItem 
@@ -29,7 +29,7 @@ const TransactionList = props => {
             })} 
           </tbody>
         </table>
-        <B btnType='primary' onClick={props.fetchNextPage}>More</B>
+        <B btnType='primary' onClick={props.fetchNextPage}>Next</B>
       </div>
     </React.Fragment>
   )
--- client/src/components/Transactions/TxListItemDebit.js
+++ client/src/components/Transactions/TxListItemDebit.js
@@ -1,32 +1,32 @@
 import React from "react";
 import Moment from 'react-moment';
+import { Link } from 'react-router-dom'
 
 const TxListItemDebit = props => {
   const date = props.transaction.createdAt && props.transaction.createdAt.slice(0, 10);
-  
+  let path;
   return (
     <React.Fragment>
-      
       {props.transaction.txOutputs.map(output => {
-        // console.log(output)
-        {/*NEED TO ADD this check to the if statement below: output.toUser._id===props.userId) */}
+        if (props.transaction.txType==='comment-vote'){
+          //Todo: Not sure how to direct page to the specific comment within the post. For now it reverts to the post
+          // path = `/comments/${output.comment}`
+        } else if (props.transaction.txType==='purchase'|| props.transaction.txType==='post-vote') {
+          path = `/posts/${props.transaction.postId}`
+        } 
+        {/* Todo: NEED TO ADD this check to the if statement below: output.toUser._id===props.userId) */}
         if (output.isListokaAcct===false) {
           return ( 
             <tr key={output._id}className='flex w-full text-red text-xs'>
               <td className='p-1 w-1/4'><Moment format="MM-DD-YYYY">{date}</Moment></td>
-              <td className='p-1 w-1/4'>{props.transaction.txType}</td>
-              <td className='p-1 w-1/4'>${output.amount.toFixed(2)}</td>
+              <td className='p-1 w-1/4 text-red'>${output.amount.toFixed(2)}</td>
+              <td className='p-1 w-1/4'><Link to={{pathname: path}} className='no-underline cursor-pointer text-red'>{props.transaction.txType}</Link></td>
               <td className='p-1 w-1/4'>{output.toUser.username}</td>
+              {/* Todo: NEED TO ADD ABOVE <Link to={{pathname: `/users/${props.transaction.toUser._id}`}}> */}
             </tr>
           )
         } return null
       })}
-      {/* <tr className='flex w-full text-red text-xs'>
-        <td className='p-1 w-1/4'><Moment format="MM-DD-YYYY">{date}</Moment></td>
-        <td className='p-1 w-1/4'>{props.transaction.txType}</td>
-        <td className='p-1 w-1/4'>${amount.toFixed(2)}</td>
-        <td className='p-1 w-1/4 '>{props.transaction.fromUser.username}</td>
-      </tr> */}
     </React.Fragment>
   )
 }
