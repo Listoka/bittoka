ff939a050b815724b8b96e5fb45fc1b41a2faff0
rob <robbyphillips@gmail.com>
2018-12-27 19:27:26 -0600

    fix warning - unused vars

--- client/src/pages/Main/MainPage.js
+++ client/src/pages/Main/MainPage.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { List, PostListItem } from '../../components/List';
 import { B } from '../../components/Widgets/index';
 import AuthUserContext from '../../components/AuthUserSession/AuthUserContext';
 import IntroHeader from '../../components/IntroHeader';
